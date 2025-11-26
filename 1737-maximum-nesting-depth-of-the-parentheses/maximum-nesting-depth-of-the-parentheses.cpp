2e05206e6c8eb60fc27a209fde3e3317b5236122
Anshul Kumar <72247841+PhEnOMeoN-47@users.noreply.github.com>
2025-11-26 20:13:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (58.53%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<int> st;
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(') st.push(i);
+            else if(s[i]==')') st.pop();
+
+            int sz = st.size();
+            ans = max(ans,sz);
+        }
+        return ans;
+    }
+};
