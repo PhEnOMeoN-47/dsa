5ebe07d662d4db00bf31abd9e793859a4ffb9f82
Anshul Kumar <72247841+PhEnOMeoN-47@users.noreply.github.com>
2026-01-14 02:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (53.02%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // better : hashmap
+        /*
         map<int, int> mpp;
         int more = 0;
         for(int i = 0; i < nums.size(); i++){
@@ -12,6 +14,32 @@ public:
             mpp[a] = i;
         }
     return {1, mpp[more]};
+    */
+
+        // optimal : 2 pointers but here we need to sort using vector<pair<int,int>>
+        // which will result TC = O(N logN)
+        int sum = 0;
+        int n = nums.size();
+        int left = 0, right = n-1;
+        vector<pair<int,int>> v;
+        for (int i = 0; i < n; i++) {
+            v.push_back({nums[i], i});
+        }
+        sort(v.begin(), v.end());
+
+        while(left < right){
+            sum = v[left].first + v[right].first;   //first has element in pair dataset
+            if(sum == target){
+                return {v[left].second, v[right].second};  //second has index in pair dataset
+            }
+            else if(sum<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return {};
     }
         
 };
